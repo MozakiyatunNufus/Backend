095cf43c2634532920442bae9ef56a4e8d9777e7
MozakiyatunNufus <mozakiyatunnufus05@gmail.com>
2025-12-02 15:53:05 +0700

    Delete Materi 1 directory

--- Materi 1/coba_1.py
+++
@@ -1,4 +0,0 @@
-print("Hello World!")
-print("Belajar Python dari Nol")
-nama = "Stikom Banyuwangi"
-print (nama)
--- Materi 1/coba_2.py
+++
@@ -1,2 +0,0 @@
-print("Hello"); print("World"); print("Tutorial Python untuk Pemula")
-nama_depan = "Stikom"; nama_belakang = "Banyuwangi"
